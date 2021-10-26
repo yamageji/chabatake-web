b36cbfb0a0c9e3300fd579ed92da5716853fed0e
K-shigehito <example@test.com>
2021-10-26 15:13:34 +0900

    ページネーションのスタイル修正 #15

--- components/BasePagination.vue
+++ components/BasePagination.vue
@@ -1,34 +1,119 @@
 <template>
-  <div class="border-t-[1px] border-warmGray-400 wrapper">
-    <ul class="pager">
-      <li v-if="current > 1" class="page arrow">
-        <nuxt-link :to="getPath(current - 1)">
+  <div
+    class="
+      pt-[32px]
+      border-t-[1px] border-warmGray-400
+      dark:border-warmGray-600
+      font-source font-bold
+      md:pt-[44px]
+    "
+  >
+    <ul class="flex flex-wrap justify-center items-center">
+      <li
+        v-if="current > 1"
+        class="w-[32px] h-[32px] rounded-[100%] m-[4px] mx-[8px]"
+      >
+        <nuxt-link
+          :to="getPath(current - 1)"
+          class="
+            flex
+            justify-center
+            items-center
+            h-full
+            text-warmGray-700
+            hover:text-warmGray-500
+            dark:hover:text-warmGray-400 dark:text-warmGray-200
+          "
+        >
           <font-awesome-icon :icon="['fas', 'chevron-left']" />
         </nuxt-link>
       </li>
-      <li v-if="3 < current" class="page">
-        <nuxt-link :to="getPath(1)"> 1 </nuxt-link>
+      <li v-if="3 < current" class="w-[32px] h-[32px] rounded-[100%] m-[4px]">
+        <nuxt-link
+          :to="getPath(1)"
+          class="
+            flex
+            justify-center
+            items-center
+            h-full
+            text-warmGray-700
+            hover:text-warmGray-500
+            dark:hover:text-warmGray-400 dark:text-warmGray-200
+          "
+        >
+          1
+        </nuxt-link>
       </li>
-      <li v-if="4 < current" class="omission">...</li>
+      <li
+        v-if="4 < current"
+        class="text-warmGray-700 my-[4px] mx-[12px] dark:text-warmGray-200"
+      >
+        ...
+      </li>
       <li
         v-for="(p, index) in pager"
         v-show="current - 3 <= p && p <= current + 1"
         :key="index"
-        class="page"
-        :class="{ active: current === p + 1 }"
+        class="w-[32px] h-[32px] rounded-[100%] m-[4px]"
+        :class="[activeClass1(p)]"
       >
-        <nuxt-link :to="getPath(p + 1)">
+        <nuxt-link
+          :to="getPath(p + 1)"
+          class="
+            flex
+            justify-center
+            items-center
+            h-full
+            text-warmGray-700
+            hover:text-warmGray-500
+            dark:hover:text-warmGray-400 dark:text-warmGray-200
+          "
+          :class="[activeClass2(p)]"
+        >
           {{ p + 1 }}
         </nuxt-link>
       </li>
-      <li v-if="current + 3 < pager.length" class="omission">...</li>
-      <li v-if="current + 2 < pager.length" class="page">
-        <nuxt-link :to="getPath(pager.length)">
+      <li
+        v-if="current + 3 < pager.length"
+        class="text-warmGray-700 my-[4px] mx-[12px] dark:text-warmGray-200"
+      >
+        ...
+      </li>
+      <li
+        v-if="current + 2 < pager.length"
+        class="w-[32px] h-[32px] rounded-[100%] m-[4px]"
+      >
+        <nuxt-link
+          :to="getPath(pager.length)"
+          class="
+            flex
+            justify-center
+            items-center
+            h-full
+            text-warmGray-700
+            hover:text-warmGray-500
+            dark:hover:text-warmGray-400 dark:text-warmGray-200
+          "
+        >
           {{ pager.length }}
         </nuxt-link>
       </li>
-      <li v-if="current < pager.length" class="page arrow">
-        <nuxt-link :to="getPath(current + 1)">
+      <li
+        v-if="current < pager.length"
+        class="w-[32px] h-[32px] rounded-[100%] m-[4px] mx-[8px]"
+      >
+        <nuxt-link
+          :to="getPath(current + 1)"
+          class="
+            flex
+            justify-center
+            items-center
+            h-full
+            text-warmGray-700
+            hover:text-warmGray-500
+            dark:hover:text-warmGray-400 dark:text-warmGray-200
+          "
+        >
           <font-awesome-icon :icon="['fas', 'chevron-right']" />
         </nuxt-link>
       </li>
@@ -67,51 +152,22 @@ export default defineComponent({
       }
     };
 
-    return { getPath };
+    const activeClass1 = (p) => {
+      return props.current === p + 1
+        ? 'bg-warmGray-700 dark:bg-warmGray-200 '
+        : '';
+    };
+    const activeClass2 = (p) => {
+      return props.current === p + 1
+        ? 'text-warmGray-200 hover:text-warmGray-200 dark:text-warmGray-700 dark:hover:text-warmGray-600'
+        : '';
+    };
+
+    return {
+      getPath,
+      activeClass1,
+      activeClass2,
+    };
   },
 });
 </script>
-
-<style lang="scss" scoped>
-.wrapper {
-  padding-top: 16px;
-}
-.pager {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  padding: 40px 0 0;
-}
-.omission {
-  color: #44403c;
-  margin: 4px 12px;
-}
-.page {
-  width: 32px;
-  height: 32px;
-  border-radius: 100%;
-  margin: 4px;
-
-  &.arrow {
-    margin: 4px 8px;
-  }
-  &.active {
-    background-color: #44403c;
-    a,
-    a:hover {
-      color: white;
-    }
-  }
-  a {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    color: #44403c;
-    &:hover {
-      color: #a8a29e;
-    }
-  }
-}
-</style>
