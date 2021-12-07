632766a9f5a4f1cabea0275c8dcc9b6e7da7b3ab
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2021-12-07 22:48:31 +0900

    fix:twitterのシェアボタンの実装

--- components/BaseSnsShareButton.vue
+++ components/BaseSnsShareButton.vue
@@ -20,36 +20,36 @@
         "
         >share</span
       >
-      <button type="button" class="">
-        <a :href="twitterURL" target="_blank" rel="noopener">
-          <span
-            class="
-              flex
-              items-center
-              justify-center
-              w-[28px]
-              h-[28px]
-              text-warmGray-100
-              bg-warmGray-700
-              rounded-full
-              hover:opacity-80
-              dark:text-warmGray-700 dark:bg-warmGray-200
-            "
-          >
-            <BaseIcon width="16" height="16" :icon-name="'twitter'">
-              <IconTwitter />
-            </BaseIcon>
-          </span>
-        </a>
-      </button>
+      <a :href="twitterURL" target="_blank" rel="noopener">
+        <span
+          class="
+            flex
+            items-center
+            justify-center
+            w-[28px]
+            h-[28px]
+            text-warmGray-100
+            bg-warmGray-700
+            rounded-full
+            hover:opacity-80
+            dark:text-warmGray-700 dark:bg-warmGray-200
+          "
+        >
+          <BaseIcon width="16" height="16" :icon-name="'twitter'">
+            <IconTwitter />
+          </BaseIcon>
+        </span>
+      </a>
     </div>
   </div>
 </template>
 
-<script>
+<script lnag="ts">
+import { computed, defineComponent, useContext } from '@nuxtjs/composition-api';
 import BaseIcon from './BaseIcon.vue';
 import IconTwitter from './icons/IconTwitter.vue';
-export default {
+
+export default defineComponent({
   name: 'BaseSnsShareButton',
   components: { BaseIcon, IconTwitter },
   props: {
@@ -58,16 +58,19 @@ export default {
       default: '',
     },
   },
-  computed: {
-    url() {
-      return `https://chabatake-web.com${this.$route.path}`;
-    },
-    textAndHashTag() {
-      return encodeURIComponent(`${this.text}`);
-    },
-    twitterURL() {
-      return `https://twitter.com/intent/tweet?url=${this.url}&text=${this.textAndHashTag}`;
-    },
+  setup(props) {
+    const { route } = useContext();
+    const path = route.value.path;
+
+    const url = `https://chabatake-web.com${path}`;
+    const textAndHashTag = encodeURIComponent(`${props.text}`);
+    const twitterURL = computed(
+      () => `https://twitter.com/intent/tweet?url=${url}&text=${textAndHashTag}`
+    );
+
+    return {
+      twitterURL,
+    };
   },
-};
+});
 </script>
