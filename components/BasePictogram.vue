9020482d6292e97a49c5c480d6fb47a39b801000
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2022-05-02 01:10:06 +0900

    fix: ピクトグラムのコンポーネント化

---
+++ components/BasePictogram.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+import BasePictoFrame from './pictograms/BasePictoFrame.vue';
+import PictoShimantoGawa from './pictograms/PictoShimantoGawa.vue';
+import PictoTaiyaki from './pictograms/PictoTaiyaki.vue';
+
+export default defineComponent({
+  name: 'BasePictogram',
+  components: {
+    BasePictoFrame,
+    PictoShimantoGawa,
+    PictoTaiyaki,
+  },
+  props: {
+    pictogram: {
+      type: String,
+      default: '',
+    },
+    width: {
+      type: [Number, String],
+      default: 113.39,
+    },
+    height: {
+      type: [Number, String],
+      default: 113.39,
+    },
+  },
+});
+</script>
+
+<template>
+  <BasePictoFrame :width="width" :height="height">
+    <PictoShimantoGawa v-if="pictogram === 'shimanto-gawa'" />
+    <PictoTaiyaki v-else-if="pictogram === 'taiyaki'" />
+  </BasePictoFrame>
+</template>
---
+++ components/pictograms/BasePictoFrame.vue
@@ -0,0 +1,45 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    x="0px"
+    y="0px"
+    viewBox="0 0 113.39 113.39"
+    :aria-labelledby="iconName"
+    role="presentation"
+  >
+    <title :id="iconName" lang="ja">{{ iconName }} icon</title>
+    <g :fill="iconColor">
+      <slot />
+    </g>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  name: 'BaseIcon',
+  props: {
+    iconName: {
+      type: String,
+      default: '',
+    },
+    width: {
+      type: [Number, String],
+      default: 113.39,
+    },
+    height: {
+      type: [Number, String],
+      default: 113.39,
+    },
+    iconColor: {
+      type: String,
+      default: 'currentColor',
+    },
+  },
+});
+</script>
+
+<style scoped></style>
