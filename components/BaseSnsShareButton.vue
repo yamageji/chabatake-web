55bcbb370e07b72e14e2cbd4c03b70c6786264b0
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2022-05-03 23:49:00 +0900

    fix: ツイッターアイコンのid属性の修正

--- components/BaseSnsShareButton.vue
+++ components/BaseSnsShareButton.vue
@@ -2,40 +2,17 @@
   <!-- Twitter share button -->
   <div>
     <div
-      class="
-        flex
-        gap-[12px]
-        items-center
-        justify-center
-        py-[8px]
-        font-bold
-        text-[17px] text-center
-      "
+      class="flex gap-[12px] items-center justify-center py-[8px] font-bold text-[17px] text-center"
     >
       <span
-        class="
-          pr-[12px]
-          border-r-[1px] border-warmGray-400
-          dark:border-warmGray-200
-        "
+        class="pr-[12px] border-r-[1px] border-warmGray-400 dark:border-warmGray-200"
         >share</span
       >
       <a :href="twitterURL" target="_blank" rel="noopener">
         <span
-          class="
-            flex
-            items-center
-            justify-center
-            w-[28px]
-            h-[28px]
-            text-warmGray-100
-            bg-warmGray-700
-            rounded-full
-            hover:opacity-80
-            dark:text-warmGray-700 dark:bg-warmGray-200
-          "
+          class="flex items-center justify-center w-[28px] h-[28px] text-warmGray-100 bg-warmGray-700 rounded-full hover:opacity-80 dark:text-warmGray-700 dark:bg-warmGray-200"
         >
-          <BaseIcon width="16" height="16" :icon-name="'twitter'">
+          <BaseIcon width="16" height="16" :icon-name="'twitter'" :icon-id="10">
             <IconTwitter />
           </BaseIcon>
         </span>
